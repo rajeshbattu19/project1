2027edcd42ddfa1e44d6132c22620fa55c594db2
RAJESH BATTU <132243356+rajeshbattu19@users.noreply.github.com>
2024-04-13 09:16:47 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Navbar';
+import Page1 from './Page1';
+import Pr from './Pr';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Page1/>
+      <Pr/>
+    </div>
+  );
+}
+
+export default App;
